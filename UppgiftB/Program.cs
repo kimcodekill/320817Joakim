e600ced4e0752b7034afdf02914fd4dc32712866
Joakim Linna <joakimlinna1998@gmail.com>
2017-09-01 12:52:42 +0200

    Added UppgiftB

---
+++ UppgiftB/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UppgiftB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en mening.");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+            string[] wordsSorted = new string[words.Length];
+
+            Array.Sort(words);
+
+            foreach (string s in words)
+                Console.WriteLine(s);
+
+            Array.Reverse(words);
+
+            foreach (string s in words)
+                Console.WriteLine(s);
+        }
+    }
+}
