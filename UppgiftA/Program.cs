0d65667a2ce5eefeeca00520095cf10744463b63
Joakim Linna <joakimlinna1998@gmail.com>
2017-09-01 10:30:31 +0200

    added UppgA

---
+++ UppgiftA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UppgiftA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en mening.");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+            Console.WriteLine(words.Length);
+
+            foreach (string s in words)
+                Console.WriteLine(s + " (" + s.Length + ')');
+        }
+    }
+}
