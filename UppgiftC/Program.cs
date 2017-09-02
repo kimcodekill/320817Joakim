b75747e9a4bacfdc8854264e37b00c2adceceee1
Joakim Linna <joakimlinna1998@gmail.com>
2017-09-02 12:38:47 +0200

    added uppgiftC

---
+++ UppgiftC/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UppgiftC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en mening.");
+            string[] words = Console.ReadLine().Split(' ');
+            string[] newWords;
+            
+            do
+            {
+                int i = 0;
+                string newWordsString = string.Empty;
+                foreach (string s in words)
+                {
+                    if (!newWordsString.Contains(s))
+                    {
+                        newWordsString += s + ' ';
+                    }
+                }
+            }
+        }
+    }
+}
