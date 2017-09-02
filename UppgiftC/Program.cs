5710272899e165840a1ebc16e9771678ad1e8e9c
Joakim Linna <joakimlinna1998@gmail.com>
2017-09-02 13:01:47 +0200

    Added comment to uppgC

--- UppgiftC/Program.cs
+++ UppgiftC/Program.cs
@@ -12,20 +12,39 @@ namespace UppgiftC
         {
             Console.WriteLine("Mata in en mening.");
             string[] words = Console.ReadLine().Split(' ');
-            string[] newWords;
-            
-            do
+
+            /*  Hade velat använda list här
+             *  
+             *  List<string> newWords = new List<string>();
+             *  
+             *  foreach (string s in words)
+             *      if (!newWords.Contains(s)
+             *          newWords.Add(s);
+             *  
+             *  int[] wordCount = new int[newWords.Count]
+             *  foreach (string s in newWords)
+             *  {
+             *      int count = 0;
+             *      foreach (string d in words)
+             *          if (s = d)
+             *              count++;
+             *      wordCount[newWords.IndexOf(s)] = count;
+             *  }
+             *  
+             */
+            string newWordsString = string.Empty;
+            foreach (string s in words)
             {
-                int i = 0;
-                string newWordsString = string.Empty;
-                foreach (string s in words)
+                if (!newWordsString.Contains(s))
                 {
-                    if (!newWordsString.Contains(s))
-                    {
-                        newWordsString += s + ' ';
-                    }
+                    newWordsString += s + '.';
                 }
             }
+
+            string[] newWords = newWordsString.Split('.');
+
+            //foreach (string s in newWords)
+            //    Console.WriteLine(s + " (" + wordCount[newWords.IndexOf(s)] + ')');
         }
     }
 }
